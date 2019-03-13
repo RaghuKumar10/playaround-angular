39346aae9827a92d6b113d1ffecc3ac6b81a0de6
Raghu Madhuswamy <raghu.madhuswamy@SPI.COM>
2019-03-13 15:39:16 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductsComponent } from './products/products.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductdetailComponent } from './products/productdetail/productdetail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component : HomeComponent},
+  {path : 'products', component: ProductsComponent},
+  {path : 'products/:id', component: ProductdetailComponent},
+  {path : 'cart', component : CartComponent},
+  {path : '**', component : NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductdetailComponent } from './products/productdetail/productdetail.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './order/checkout/checkout.component';
+import { OrderConfirmationComponent } from './order/order-confirmation/order-confirmation.component';
+import { OrdersComponent } from './order/orders/orders.component';
+import { BannerComponent } from './home/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NotFoundComponent,
+    ProductsComponent,
+    ProductdetailComponent,
+    CartComponent,
+    CheckoutComponent,
+    OrderConfirmationComponent,
+    OrdersComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CarouselModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
